39c62a6aad13bed391672e28278d2414084655bd
Poolezv <poolezv@clarkson.edu>
2022-09-01 08:43:10 -0400

    First commit

---
+++ HelloMain.java
@@ -0,0 +1,13 @@
+public class HelloMain {
+    /***
+     * This is out main method.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+         This is the first command
+         This is the second command
+         */
+        System.out.println("Hello World");
+    }
+}
